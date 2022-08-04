6ebc493dc728286f169e60f997e076f4e9f2bcc8
SMOK <71714688+Vampir20@users.noreply.github.com>
2022-08-04 11:11:58 +0200

    Add files via upload

--- app/src/main/java/com/example/map/MapsActivity.kt
+++ app/src/main/java/com/example/map/MapsActivity.kt
@@ -1,14 +1,15 @@
 package com.example.map
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.map.databinding.ActivityMapsBinding
+import android.os.Bundle
+
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.example.map.databinding.ActivityMapsBinding
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -23,46 +24,25 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
+                .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
-
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
+        // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-
-        val moscow = LatLng(55.7, 37.6)
-        mMap.addMarker(MarkerOptions().position(moscow).title("Marker in Moscow"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(moscow))
-
-        val dubai = LatLng(25.2, 55.2)
-        mMap.addMarker(MarkerOptions().position(dubai).title("Marker in Dubai"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(dubai))
-
-        val london = LatLng(51.5, -0.1)
-        mMap.addMarker(MarkerOptions().position(london).title("Marker in London"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(london))
-
-        val dmitrov = LatLng(56.3, 56.3)
-        mMap.addMarker(MarkerOptions().position(dmitrov).title("Marker in Dmitrov"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(dmitrov))
-
-        val la = LatLng(34.0, -118.2)
-        mMap.addMarker(MarkerOptions().position(la).title("Marker in LA"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(la))
-
-        val cologne = LatLng(50.9, 6.9)
-        mMap.addMarker(MarkerOptions().position(cologne).title("Marker in Cologne"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(cologne))
-
-        val miami = LatLng(25.7, -80.1)
-        mMap.addMarker(MarkerOptions().position(miami).title("Marker in Miami"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(miami))
-
     }
-
 }
